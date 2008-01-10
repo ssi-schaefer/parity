b6146eaa3b7f24e1417c8d62c50f3f9c13266326
mduft <mduft@26386e8f-9d3d-0410-aaca-840ac0c00a30>
2008-01-10 15:23:43 +0000

    arg
    git-svn-id: https://svn.code.sf.net/p/parity/code/trunk@50 26386e8f-9d3d-0410-aaca-840ac0c00a30

---
+++ parity.graphical.configurator/Mapping.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <Context.h>
+
+System::String^ FormatForDisplay(const bool& val);
+System::String^ FormatForDisplay(const long& val);
+System::String^ FormatForDisplay(const std::string& val);
+System::String^ FormatForDisplay(const parity::utils::Path& val);
+System::String^ FormatForDisplay(const parity::utils::DefineMap& val);
+System::String^ FormatForDisplay(const parity::utils::PathVector& val);
+System::String^ FormatForDisplay(const parity::utils::LanguageType& val);
+System::String^ FormatForDisplay(const parity::utils::SubsystemType& val);
+System::String^ FormatForDisplay(const parity::utils::ToolchainType& val);
+System::String^ FormatForDisplay(const parity::utils::RuntimeType& val);
+System::String^ FormatForDisplay(const parity::utils::Color::ColorMode& val);
+
+typedef System::String^ (*DisplayFormatFunction)(parity::utils::Context& ctx);
+typedef bool (*IsDefaultFunction)(parity::utils::Context& ctx);
+
+struct MappingStruct
+{
+	char* Name;
+	char* Type;
+	char* Default;
+	DisplayFormatFunction Formatter;
+	IsDefaultFunction IsDefault;
+};
+
+extern DisplayMappingStruct* DisplayMapping;
