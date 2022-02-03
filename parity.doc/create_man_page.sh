1726354c8a043f30bb050514bd3938d260142da6
Mario Mattl <mario.mattl@ssi-schaefer.com>
2022-02-03 13:10:43 +0100

    parity-2.0.3: Added support for VS 2022 (VC17)
    Change-Id: If2083c502d364d326460265261a9e7bdb55d0424

--- parity.doc/create_man_page.sh
+++ parity.doc/create_man_page.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-pod2man --section=1 --center "Parity, The Compiler Wrapper" --release "2.0.2" parity.pod > parity.1
+pod2man --section=1 --center "Parity, The Compiler Wrapper" --release "2.0.3" parity.pod > parity.1
